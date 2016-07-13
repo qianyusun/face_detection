63b3f969b89e17ad8b5bc7cfd4787af2d47aa645
Hang <hangy@umich.edu>
2016-07-13 14:16:54 +0800

    fix bugs

--- Utility.h
+++ Utility.h
@@ -10,7 +10,7 @@
 
 
 int vectorsum(std::vector<int> v);
-std::vector<Rect> ReadRectInfo(std::string folder, std::string filename, std::string image_name);
+std::vector<cv::Rect> ReadRectInfo(std::string folder, std::string filename, std::string image_name);
 void draw_rects(cv::Mat *frame, std::vector<cv::Rect> faces, int color, int thickness, int lineType);
 bool overlap_bool(cv::Rect rectA, std::vector<cv::Rect> answer, int threshold );
 std::vector<std::string> getImageNames (void);
