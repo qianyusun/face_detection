e14305845df7823440b7d86ec1b3c17c06fd4f71
Hang <hangy@umich.edu>
2016-07-13 14:07:01 +0800

    decouple functions

--- Utility.h
+++ Utility.h
@@ -1,14 +1,18 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <iosfwd>
 #include <vector>
-class Rect;
+
 
 int vectorsum(std::vector<int> v);
 std::vector<Rect> ReadRectInfo(std::string folder, std::string filename, std::string image_name);
-void draw_rects(Mat *frame, std::vector<Rect> faces, int color, int thickness, int lineType);
-bool overlap_bool(Rect rectA, std::vector<Rect> answer, int threshold );
-std::vector<String> getImageNames (void);
+void draw_rects(cv::Mat *frame, std::vector<cv::Rect> faces, int color, int thickness, int lineType);
+bool overlap_bool(cv::Rect rectA, std::vector<cv::Rect> answer, int threshold );
+std::vector<std::string> getImageNames (void);
 
 #endif
--- recall_rate.h
+++ recall_rate.h
@@ -1,13 +1,12 @@
 #ifndef RECALL_RATE_H
 #define RECALL_RATE_H
+
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <vector>
-class Rect;
-class Mat;
 
-
-void evaluate_recall_rate(std::vector<Rect> faces,std::vector<Rect> faces_ans, Mat *frame);
-void evaluate_pos_neg_rate(std::vector<Rect> faces,std::vector<Rect> faces_ans, Mat *frame);
-
-
-
+void evaluate_recall_rate(std::vector<cv::Rect> faces,std::vector<cv::Rect> faces_ans, cv::Mat *frame);
+void evaluate_pos_neg_rate(std::vector<cv::Rect> faces,std::vector<cv::Rect> faces_ans, cv::Mat *frame);
 #endif
