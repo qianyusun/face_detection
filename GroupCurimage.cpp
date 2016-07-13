9744ec1a17e4a0256fa81f861a323a0da64d7048
Hang <hangy@umich.edu>
2016-07-13 19:19:32 +0800

    fix bugs

--- GroupCurimage.cpp
+++ GroupCurimage.cpp
@@ -1,10 +1,5 @@
 #include "Utility.h"
 
-#include "opencv2/objdetect.hpp"
-#include "opencv2/videoio.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -69,11 +64,11 @@ int main( int argc, char** argv )
     // moveRect(ResultFaces);
 
     //Cut rectangles
-    CutRect(ResultFaces, &frame);
+    CutRect(argv[1] ,ResultFaces, &frame);
     
     //Display the window
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow( "Display Image", frame );  
+    // namedWindow("Display Image", WINDOW_AUTOSIZE );
+    // imshow( "Display Image", frame );  
 
 	waitKey(0);
 	return 0;
