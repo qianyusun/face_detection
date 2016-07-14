b2b71e19722012a408037dbf67481097f98bc4ac
Hang <hangy@umich.edu>
2016-07-14 11:35:45 +0800

    fix bugs

--- utility.h
+++ utility.h
@@ -13,7 +13,7 @@ extern std::string test_data_folder;
 extern std::string test_info;
 
 int vectorsum(std::vector<int> v);
-void ReadImagesInfo(std::string file_path, std::unordered_map<std::sting, std::vector<cv::Rect>>&);
+void ReadImagesInfo(std::string file_path, std::unordered_map<std::string, std::vector<cv::Rect>>&);
 void draw_rects(cv::Mat *frame, std::vector<cv::Rect> faces, int color, int thickness, int lineType);
 bool overlap_bool(cv::Rect rectA, std::vector<cv::Rect> answer, int threshold );
 std::vector<std::string> getImageNames (void);
